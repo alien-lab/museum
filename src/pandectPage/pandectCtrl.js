7574260333d81b834c0f562c5839a0eaf0576544
AppAnge1008 <812474076@qq.com>
2017-09-23 10:38:56 +0800

    init commit

---
+++ src/pandectPage/pandectCtrl.js
@@ -0,0 +1,58 @@
+/**
+ * Created by asus on 2017/9/20.
+ */
+(function () {
+    'use strict';
+    var app=angular.module("pandectApp",[]);
+
+    app.controller("pandectCtrl",["$scope","paperContentService",function ($scope,paperContentService) {
+
+        $scope.paperList = [];
+
+        paperContentService.loadpaperList(function (data,flag) {
+            if(!flag){
+                // alert(data);
+            }
+            $scope.paperList = data;
+            console.log($scope.paperList);
+        });
+
+
+        $scope.slides = [{
+            "image":'img/1_1_1.png'
+        },{
+            "image":'img/1_1_2.png'
+        },{
+            "image":'img/1_1_3.png'
+        },{
+            "image":'img/1_1_4.png'
+        },{
+            "image":'img/1_1_5.png'
+        },{
+            "image":'img/1_1_6.png'
+        }];
+
+
+
+
+
+
+    }]);
+
+    app.service("paperContentService",["$http",function ($http) {
+        this.loadpaperList = function (callback) {
+            $http({
+                url:'data.json',
+                method:'GET'
+            }).then(function (data) {
+                if(callback){
+                    callback(data.data,true);
+                }
+            },function (error) {
+                if(callback){
+                    callback(error,false);
+                }
+            });
+        }
+    }])
+})();
