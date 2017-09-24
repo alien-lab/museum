b52767aaecfc68dfa4f9f3fd4c201291669f083a
AppAnge1008 <812474076@qq.com>
2017-09-24 22:53:14 +0800

    单元内容的导航栏

--- src/pandectPage/pandectCtrl.js
+++ src/pandectPage/pandectCtrl.js
@@ -19,17 +19,17 @@
 
 
         $scope.slides = [{
-            "image":'img/1_1_1.png'
+            "image":'img/1_1_1_1.png'
         },{
-            "image":'img/1_1_2.png'
+            "image":'img/1_1_1_2.png'
         },{
-            "image":'img/1_1_3.png'
+            "image":'img/1_1_1_3.png'
         },{
-            "image":'img/1_1_4.png'
+            "image":'img/1_1_1_4.png'
         },{
-            "image":'img/1_1_5.png'
+            "image":'img/1_1_1_3.png'
         },{
-            "image":'img/1_1_6.png'
+            "image":'img/1_1_1_4.png'
         }];
 
 
