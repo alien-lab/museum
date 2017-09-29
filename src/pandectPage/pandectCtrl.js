bd4fec9505910590007281945a4dc5dd00bcb9f2
juhuiguang <juhuiguang@foxmail.com>
2017-09-29 16:47:37 +0800

    第一部分静态页面

--- src/pandectPage/pandectCtrl.js
+++ src/pandectPage/pandectCtrl.js
@@ -3,39 +3,70 @@
  */
 (function () {
     'use strict';
-    var app=angular.module("pandectApp",[]);
+    var app=angular.module("pandectApp",["ui.router","ui.bootstrap"]);
 
-    app.controller("pandectCtrl",["$scope","paperContentService",function ($scope,paperContentService) {
+    app.controller("pandectCtrl",["$scope","paperContentService","$state","$location","$uibModal",
+        function ($scope,paperContentService,$state,$location,$uibModal) {
 
         $scope.paperList = [];
+        var absurl=$location.absUrl();
+        console.log(absurl);
+        var pos=absurl.indexOf("?part=");
+        if(pos>0){
+            $scope.partNumber=absurl.substring(pos+6,pos+7);
+        }
+
+        $scope.pageback=function(){
+            console.log("back");
+            window.location.href="/index/index.html#sec";
+        }
 
         paperContentService.loadpaperList(function (data,flag) {
             if(!flag){
                 // alert(data);
             }
-            $scope.paperList = data;
+            console.log($scope.partNumber);
+            if($scope.partNumber){
+                var newdata=new Array();
+                newdata.push(data[$scope.partNumber-1]);
+                $scope.paperList = newdata;
+            }
+
             console.log($scope.paperList);
         });
 
-
-        $scope.slides = [{
-            "image":'img/1_1_1_1.png'
-        },{
-            "image":'img/1_1_1_2.png'
-        },{
-            "image":'img/1_1_1_3.png'
-        },{
-            "image":'img/1_1_1_4.png'
-        },{
-            "image":'img/1_1_1_3.png'
-        },{
-            "image":'img/1_1_1_4.png'
-        }];
+        $scope.modalOpen=function(info){
+            var modalInstance=$uibModal.open({
+                templateUrl:"contentInfo.html",
+                controller:'contentInfoController',
+                resolve:{
+                    info:info
+                }
+            });
+        }
 
 
 
 
+    }]);
 
+    app.controller("contentInfoController",["$scope","$uibModalInstance","info",
+        function($scope,$uibModalInstance,info){
+        console.log("info",info);
+
+        $scope.info=info;
+        if(info.infoTitle.indexOf("0-")>=0){
+            $scope.info.infoTitle=info.infoTitle.substring(2);
+            $scope.direction=0
+        }else{
+            $scope.direction=1
+        }
+        $scope.textarr=info.infoText.split("$$");
+
+
+        $scope.ok=function(){
+            $uibModalInstance.dismiss("cancel");
+        }
 
     }]);
 
